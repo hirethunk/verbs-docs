b22d262f8f627fb14b917743f08022f883ac20da
Chris Morrell <inxilpro@users.noreply.github.com>
2023-11-06 17:05:37 -0500

    Update Navigation.php

--- app/Data/Navigation.php
+++ app/Data/Navigation.php
@@ -2,9 +2,11 @@
 
 namespace App\Data;
 
+use Illuminate\Contracts\Filesystem\FileNotFoundException;
 use Illuminate\Filesystem\Filesystem;
 use Spatie\LaravelData\Data;
 use Spatie\LaravelData\DataCollection;
+use Throwable;
 
 class Navigation extends Data
 {
@@ -13,14 +15,18 @@ class Navigation extends Data
 	
 	public function __construct(Filesystem $fs)
 	{
-		$path = storage_path('docs/main/navigation.json');
-		$data = $fs->json($path);
+		try {
+			$path = storage_path('docs/main/navigation.json');
+			$data = $fs->json($path);
+		} catch (FileNotFoundException) {
+			$data = [];
+		}
 		
 		$this->sections = NavigationSection::collection($data);
 	}
 	
 	public function section(string $slug): ?NavigationSection
 	{
-		return $this->sections->first(fn (NavigationSection $section) => $section->slug === $slug);
+		return $this->sections->first(fn(NavigationSection $section) => $section->slug === $slug);
 	}
 }
