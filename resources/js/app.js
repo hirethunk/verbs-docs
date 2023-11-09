3d23ef4618e7d1b7f3e81e35e6746e306d630707
Chris Morrell <inxilpro@users.noreply.github.com>
2023-11-09 16:53:34 -0500

    WIP

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,8 +1,44 @@
-import Alpine from 'alpinejs'
-import intersect from '@alpinejs/intersect'
+import Alpine from 'alpinejs';
+import intersect from '@alpinejs/intersect';
 
-Alpine.plugin(intersect)
+Alpine.plugin(intersect);
 
-window.Alpine = Alpine
+window.Alpine = Alpine;
 
-Alpine.start()
+Alpine.data('visibleNavHighlighter', () => ({
+	headings: [],
+	active_permalink: null,
+	
+	init() {
+		this.headings = Array.from(document.querySelectorAll('.heading-permalink'))
+			.map(node => ({
+				title: node.parentNode.textContent.replace('¶', ''),
+				permalink: node.id,
+				node: node.parentNode,
+				top: Infinity,
+			}));
+		
+		this.onScroll();
+	},
+	
+	onScroll() {
+		const threshold = window.innerHeight / 10;
+		
+		this.headings.forEach(heading => {
+			heading.top = heading.node.getBoundingClientRect().top;
+		});
+		
+		const visible_headings = this.headings
+			.filter(heading => heading.top < threshold)
+			.sort((a, b) => b.top - a.top);
+		
+		if (visible_headings.length === 0) {
+			this.active_permalink = null;
+			return;
+		}
+		
+		this.active_permalink = visible_headings[0].permalink;
+	},
+}));
+
+Alpine.start();
--- resources/views/docs/page.blade.php
+++ resources/views/docs/page.blade.php
@@ -29,14 +29,42 @@
 		<meta property="twitter:image" content="{{ asset('opengraph/'.$section->slug.'/'.$item->slug.'.png') }}">
 	</x-slot:meta>
 	
-	<article x-data class="prose max-w-none">
+	<div class="flex">
+		<article x-data class="prose max-w-none overflow-x-auto">
+			
+			<h1>
+				{{ $page->title }}
+			</h1>
+			
+			{{ $page }}
 		
-		<h1>
-			{{ $page->title }}
-		</h1>
-		
-		{{ $page }}
-		
-	</article>
-	
+		</article>
+		<aside
+			x-data="visibleNavHighlighter"
+			x-on:scroll.window.throttle.50ms="onScroll()"
+			x-show="headings.length > 0"
+			class="hidden top-16 w-64 h-screen sticky overflow-y-auto py-8 pl-6 lg:block"
+		>
+			<h4 class="mb-2 block text-sm font-bold uppercase text-slate-300">
+				On this page
+			</h4>
+			
+			<ul class="space-y-3">
+				<template x-for="heading in headings">
+					<li class="text-sm">
+						<a
+							:href="`#${heading.permalink}`"
+							class="text-slate-600 hover:text-slate-900"
+							:class="{ 
+								'font-medium text-slate-900': active_permalink === heading.permalink, 
+								'text-slate-600': active_permalink !== heading.permalink
+							}"
+							x-text="heading.title"
+						></a>
+					</li>
+				</template>
+			</ul>
+		</aside>
+	</div>
+
 </x-layout>
