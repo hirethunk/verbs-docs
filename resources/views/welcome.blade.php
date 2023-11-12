81bd327d0392548119c792b58ff9bee23a587ff2
Chris Morrell <inxilpro@users.noreply.github.com>
2023-11-12 12:57:17 -0500

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,20 +13,46 @@
     </p>
 
     <div class="grid grid-cols-2 gap-4 my-6">
-        <p class="order-1 font-semibold text-center">
-            Stop trying to cram everything in your models…
-        </p>
-        <pre class="m-0 p-0 rounded overflow-x-auto order-3">
-            <x-torchlight-code language="php" :contents="resource_path('examples/welcome/from.php')" />
-        </pre>
-    
-        <p class="order-2 font-semibold text-center">
-            And start using actual verbs…
-        </p>
-        <pre class="m-0 p-0 rounded overflow-x-auto order-4">
-            <x-torchlight-code language="php" :contents="resource_path('examples/welcome/to.php')" />
-        </pre>
+        <div>
+            <p class="order-1 font-semibold text-center">
+                Stop trying to cram everything in your models…
+            </p>
+            <div
+                x-data="{ expanded: false }"
+                class="max-h-96 overflow-hidden relative"
+                :class="{ 'max-h-96': !expanded }"
+            >
+                <pre class="m-0 p-0 rounded overflow-x-auto order-3">
+                    <x-torchlight-code language="php" :contents="resource_path('examples/welcome/from.php')" />
+                </pre>
+                <div 
+                    class="flex flex-col items-center justify-end p-4 bg-gradient-to-b from-30% from-transparent to-90% to-white absolute inset-0 z-10"
+                    :class="{ 'bg-gradient-to-b': ! expanded }"
+                >
+                    <button
+                        @click="expanded = !expanded"
+                        type="button"
+                        class="rounded-full bg-indigo-600 px-2.5 py-1 text-xs font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                        x-text="expanded ? 'Show less' : 'Show more'"
+                    >
+                        Show more
+                    </button>
+                </div>
+            </div>
+        </div>
+        <div>
+            <p class="order-2 font-semibold text-center">
+                And start using actual verbs…
+            </p>
+            <div class="max-h-96 overflow-hidden relative">
+                <pre class="m-0 p-0 rounded overflow-x-auto order-4">
+                    <x-torchlight-code language="php" :contents="resource_path('examples/welcome/to.php')" />
+                </pre>
+            </div>
+        </div>
     </div>
+    
+    <hr class="my-12" />
 
     <p class="my-6 text-center lg:text-left">
         <x-button href="{{ route('docs.section.item', ['getting-started', 'quickstart']) }}">
