ad61a95cfec5da92e14ff2174674e8413f9abd88
Chris Morrell <inxilpro@users.noreply.github.com>
2023-11-13 13:38:00 -0500

    Add instantsearch

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,52 +1,52 @@
 import Alpine from 'alpinejs';
 import intersect from '@alpinejs/intersect';
-// import docsearch from '@docsearch/js';
+import docsearch from '@docsearch/js';
 
-// docsearch({
-// 	appId: '',
-// 	apiKey: '',
-// 	indexName: '',
-// 	container: '#docsearch',
-// 	insights: true,
-// });
+docsearch({
+    appId: 'A5H99QB6AQ',
+    apiKey: 'a50c8be35deeba61f858552bd23bda77',
+    indexName: 'verbs',
+    container: '#docsearch',
+    insights: true,
+});
 
 Alpine.plugin(intersect);
 
 window.Alpine = Alpine;
 
 Alpine.data('onThisPage', () => ({
-	headings: [],
-	active_permalink: null,
-	
-	init() {
-		this.headings = Array.from(document.querySelectorAll('.heading-permalink'))
-			.map(node => ({
-				title: node.parentNode.textContent.replace('#', ''),
-				permalink: node.id,
-				node: node.parentNode,
-				level: parseInt(node.parentNode.tagName.replace(/\D/, '')),
-				top: Infinity,
-			}));
-		
-		this.onScroll();
-	},
-	
-	onScroll() {
-		this.headings.forEach(heading => {
-			heading.top = heading.node.getBoundingClientRect().top;
-		});
+    headings: [],
+    active_permalink: null,
 
-		const visible_headings = this.headings
-			.filter(heading => heading.top < 200)
-			.sort((a, b) => b.top - a.top);
+    init() {
+        this.headings = Array.from(document.querySelectorAll('.heading-permalink'))
+            .map(node => ({
+                title: node.parentNode.textContent.replace('#', ''),
+                permalink: node.id,
+                node: node.parentNode,
+                level: parseInt(node.parentNode.tagName.replace(/\D/, '')),
+                top: Infinity,
+            }));
 
-		if (visible_headings.length === 0) {
-			this.active_permalink = null;
-			return;
-		}
+        this.onScroll();
+    },
 
-		this.active_permalink = visible_headings[0].permalink;
-	},
+    onScroll() {
+        this.headings.forEach(heading => {
+            heading.top = heading.node.getBoundingClientRect().top;
+        });
+
+        const visible_headings = this.headings
+            .filter(heading => heading.top < 200)
+            .sort((a, b) => b.top - a.top);
+
+        if (visible_headings.length === 0) {
+            this.active_permalink = null;
+            return;
+        }
+
+        this.active_permalink = visible_headings[0].permalink;
+    },
 }));
 
 Alpine.start();
