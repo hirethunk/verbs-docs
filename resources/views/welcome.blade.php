12726c70f64f4483e9faa460ed0db86cce87d91f
Chris Morrell <inxilpro@users.noreply.github.com>
2023-11-13 09:22:52 -0500

    Slightly better show more UX

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,26 +12,24 @@
         Think in Verbs… not nouns.
     </p>
 
-    <div class="grid grid-cols-2 gap-4 my-6">
+    <div class="lg:grid lg:grid-cols-2 lg:gap-4 my-6">
         <div>
-            <p class="order-1 font-semibold text-center">
+            <p class="font-semibold text-center">
                 Stop trying to cram everything in your models…
             </p>
             <div
-                x-data="{ expanded: false }"
-                class="max-h-96 overflow-hidden relative"
-                :class="{ 'max-h-96': !expanded }"
+                    x-data="{ expanded: false }"
+                    class="max-h-80 relative my-6"
+                    :class="{ 'max-h-80': !expanded }"
             >
-                <pre class="m-0 p-0 rounded overflow-x-auto order-3">
-                    <x-torchlight-code 
-                        language="php" 
-                        :contents="preg_replace('/^\s*(<\?php)\s*/i', '', file_get_contents(resource_path('examples/welcome/from.php')))" 
-                    />
-                </pre>
-                <div 
-                    class="flex flex-col items-center justify-end p-4 bg-gradient-to-b from-30% from-transparent to-90% to-white absolute inset-0 z-10"
-                    :class="{ 'bg-gradient-to-b': ! expanded }"
-                >
+                <pre 
+                    class="m-0 p-0 max-h-80 overflow-hidden rounded overflow-x-auto"
+                    :class="{ 'max-h-80': !expanded }"
+                ><x-torchlight-code
+                            language="php"
+                            :contents="preg_replace('/^\s*(<\?php)\s*/i', '', file_get_contents(resource_path('examples/welcome/from.php')))"
+                    /></pre>
+                <div class="absolute w-full text-center -bottom-4 z-20">
                     <button
                         @click="expanded = !expanded"
                         type="button"
@@ -41,23 +39,25 @@
                         Show more
                     </button>
                 </div>
+                <div
+                        class="pointer-events-none flex flex-col items-center justify-end p-4 bg-gradient-to-b from-50% from-transparent to-90% to-white absolute inset-0 z-10"
+                        :class="{ 'bg-gradient-to-b': ! expanded }"
+                ></div>
             </div>
         </div>
-        <div>
-            <p class="order-2 font-semibold text-center">
+        <div class="pt-6 lg:pt-0">
+            <p class="font-semibold text-center">
                 And start using actual verbs…
             </p>
-            <div class="max-h-96 overflow-hidden relative">
-                <pre class="m-0 p-0 rounded overflow-x-auto order-4">
-                    <x-torchlight-code 
-                        language="php"
-                        :contents="preg_replace('/^\s*(<\?php)\s*/i', '', file_get_contents(resource_path('examples/welcome/to.php')))"
-                    />
-                </pre>
+            <div class="max-h-80 overflow-hidden relative my-6">
+                <pre class="rounded overflow-x-auto"><x-torchlight-code
+                            language="php"
+                            :contents="preg_replace('/^\s*(<\?php)\s*/i', '', file_get_contents(resource_path('examples/welcome/to.php')))"
+                    /></pre>
             </div>
         </div>
     </div>
-    
+
     <hr class="my-12" />
 
     <p class="my-6 text-center lg:text-left">
