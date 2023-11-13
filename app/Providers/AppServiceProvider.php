43d3f1073287121a8fcbd72665f120a521bbbe83
Chris Morrell <inxilpro@users.noreply.github.com>
2023-11-13 16:26:02 -0500

    Force HTTPS

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,27 +3,31 @@
 namespace App\Providers;
 
 use App\Data\Navigation;
-use Illuminate\Foundation\Application;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
-	public function register(): void
-	{
-		$this->app->singleton(Navigation::class, function() {
-			return new Navigation(
-				path: 'docs/main/docs/navigation.json',
-				prefix: 'docs',
-			);
-		});
-	}
-	
-	public function boot(): void
-	{
-		$this->callAfterResolving('view', function() {
-			View::share('active_item', null);
-			View::share('navigation', app(Navigation::class));	
-		});
-	}
+    public function register(): void
+    {
+        $this->app->singleton(Navigation::class, function() {
+            return new Navigation(
+                path: 'docs/main/docs/navigation.json',
+                prefix: 'docs',
+            );
+        });
+    }
+
+    public function boot(): void
+    {
+        $this->callAfterResolving('view', function() {
+            View::share('active_item', null);
+            View::share('navigation', app(Navigation::class));
+        });
+        
+        if ($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
+    }
 }
